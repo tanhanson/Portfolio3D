795c4106c8714e5f831c7b478506d25e1a577c0a
tanhanson <144844237+tanhanson@users.noreply.github.com>
2026-06-04 21:05:18 -0400

    vercel

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,1 +1,14 @@
 
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  optimizeDeps: {
+    exclude: ['@dimforge/rapier3d-compat']
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  }
+})
